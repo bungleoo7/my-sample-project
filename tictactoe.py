1d4bfbd829345b4e19f44cd3495a301bb31b3f37
Brandon Cubides <brandon_cubides@yahoo.com>
2022-09-24 01:00:38 -0600

    added some notes and adjustments

--- tictactoe.py
+++ tictactoe.py
@@ -1,35 +1,33 @@
 def main():
     player = next_player("")
     board = create_board()
-    while not (has_winner(board) or is_a_draw(board)):
+    while not (winner(board) or draw(board)):
         display_board(board)
-        make_move(player, board)
+        players_move(player, board)
         player = next_player(player)
     display_board(board)
     print("Good game. Thanks for playing!") 
 
+
+# we'll start off with creating the board first.
 def create_board():
-    board = []
-    for square in range(9):
-        board.append(square)
+    board = [] #these will act as the squares
+    for square in range(9): #this is how we'll choose our place on the board
+        board.append(square) #keeps adding a square (specifically at the end)
     return board
 
-def display_board(board):
+
+# next, the board must be displayed
+def display_board(board): #uses board as the variable since thats what we are displaying
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-+--+-')
+    print('-+-+-')
     print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-+--+-')
+    print('-+-+-')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
-    
-def is_a_draw(board):
-    for square in range(9):
-        if board[square] != "x" and board[square] != "o":
-            return False
-    return True 
-    
-def has_winner(board):
+
+def winner(board):
     return (board[0] == board[1] == board[2] or
             board[3] == board[4] == board[5] or
             board[6] == board[7] == board[8] or
@@ -39,15 +37,22 @@ def has_winner(board):
             board[0] == board[4] == board[8] or
             board[2] == board[4] == board[6])
 
-def make_move(player, board):
-    square = int(input(f"{player}'s turn to choose a square (1-9): "))
-    board[square - 1] = player
+def draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True 
 
-def next_player(current):
-    if current == "" or current == "o":
+def players_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): ")) # this is how the player will choose his spot
+    board[square - 1] = player 
+
+def next_player(currentplayer):
+    if currentplayer == "" or currentplayer == "o":
         return "x"
-    elif current == "x":
+    elif currentplayer == "x":
         return "o"
+
 
 if __name__ == "__main__":
     main()
